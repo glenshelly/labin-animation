b9c48409a98553aedd13f09e90b1d3e74baa04bb
Glen Shelly <glen.shelly@emmisolutions.com>
2018-03-25 14:13:30 -0500

    final (?) cleanup

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Animation
 
-This application determines movement of items in a linear field.  
+This application determines movement of particles in a linear field.  
 
-Full requirements are in the 'BackendCodeTest' pdf in this directory.
+Full requirements are described in Animation section of the 'BackendCodeTest' pdf in this directory.
 
 
 Building and Running
@@ -12,7 +12,7 @@ Assumptions
 -----------
 The following software is assumed to be installed and available for the building and running of this application
 - Java 8
-- Maven
+- Maven, with a junit dependency
  
 
 Building the application with Maven
@@ -23,41 +23,42 @@ Run the maven package or install phase from the root of the application:
    > mvn clean package
 ```
 
-This will create the following jar file: ./target/missing_letters.jar
+This will create the following jar file: ./target/animation.jar
 
 
 Running the unit tests
 ----------------------
 Run the included unit tests using the standard maven test phase:
 ```$xslt
-> mvn test
+> mvn clean test
 ```
 
 
 Running the application
 -----------------------
-To run the application on a single given string, execute the missing_letters jar with a input string parameter
+To run the application on a single given string, execute the animation jar with two input parameter
 ```angular2html
 
-   > java  -jar  target/missing_letters.jar  {input-string-parameter}
+   > java  -jar  target/animation.jar  {speed-parameter}  {initial-chamber-parameter}
 ```
 
-For example, to test a string such as "alert brown dog," 
-run the application as follows (using quotes to surround input with spaces): 
+For example, to test with a speed of 2 and an initial chamber configuration of ..LRL.. 
+run the application as follows: 
 ```angular2html
 
-   > java -jar target/missing_letters.jar "alert brown dog"
+   > java  -jar target/animation.jar 2 ..LRL..
 ```
 
 Generating and Testing Large Chambers
 ------------------------
 
-A helper class called ParticleSubmitter (in com.glen.aniation.generator) can be used to generate
+A helper class called ParticleSubmitter (in com.glen.animation.generator) can be used to generate
 Very Large Chambers for testing.   Adjust the values at the start of the main() method to 
 vary the contents, size and quantity of the auto-generated and submitted chambers.
 
 Using this approach on a basic development laptop provided the following preliminary benchmark: 
-a 10,000,000 character chamber with 4,000,000 particles took between 300ms and 1,300ms to process.
+a 10,000,000 character chamber with 4,000,000 particles and a speed of 500,000 (1/20 the chamber size)
+took between 300ms and 1,300ms to process.
 
 
 Notes on the Implementation
@@ -66,18 +67,19 @@ Notes on the Implementation
 Assumptions regarding the requirements
 --------------------------------------
 
-1. An bad speed (less than or equal to 0) will result in an IllegalArgumentException
-1. A null input String will result in an IllegalArgumentException
+1. An bad or missing speed (less than or equal to 0) will result in an IllegalArgumentException
+1. A null input String for chamber configuration will result in an IllegalArgumentException
 1. The values for left and right particles in the input string are case sensitive.
 
 
 Likely Improvements in real life
 --------------------------------
-Depending on the actual usage for this (a one-off script?  a production-grade application?), the
+Depending on the actual usage for this (A one-off script?  A production-grade application?), the
 following improvements might be appropriate:
 
 - Add additional tests
 - Use a real logging system (e.g., log4j)
+
 
 
 
--- src/main/java/com/glen/animation/AnimateParticles.java
+++ src/main/java/com/glen/animation/AnimateParticles.java
@@ -43,28 +43,28 @@ public class AnimateParticles {
      *  determine the location at fixed intervals (provided by the given speed)
      *
      * @param speed the amount each particle moves, per 'tick'
-     * @param inputString a depiction of the particle chamber at the start
+     * @param init a depiction of the particle chamber at the start
      * @return a String array, with the various snapshots of the particle locations after each tick o' the clock
      */
-    public String[] animate(final int speed, final String inputString) {
+    public String[] animate(final int speed, final String init) {
 
         if (speed <= 0) {
             throw new IllegalArgumentException("speed must be greater than 0");
         }
-        if (inputString == null) {
+        if (init == null) {
             throw new IllegalArgumentException("input String must not be null");
         }
 
 
         // Initialize the Left-moving-array and Right-moving-array
         // Leave space in the two arrays for the possibility of *all* of them being left moving, or right moving
-        final int inputStringLength = inputString.length();
+        final int inputStringLength = init.length();
         final int[] leftMovingArray = new int[inputStringLength];
         int activeLeftMovingParticles = 0;
         final int[] rightMovingArray = new int[inputStringLength];
         int activeRightMovingParticles = 0;
         for (int i = 0; i < inputStringLength; i++) {
-            final byte foundByte = (byte) inputString.charAt(i);
+            final byte foundByte = (byte) init.charAt(i);
             switch (foundByte) {
                 case ASCII_L_INDICATOR:
                     leftMovingArray[activeLeftMovingParticles++] = i;
@@ -86,30 +86,34 @@ public class AnimateParticles {
 
         // Main Loop
         // While there are still particles in the chamber, record their position and then move them
-        while (skipableLeftMovingParticles < activeLeftMovingParticles || skipableRightMovingParticles < activeRightMovingParticles) {
+        while (skipableLeftMovingParticles < activeLeftMovingParticles
+                || skipableRightMovingParticles < activeRightMovingParticles) {
 
             /*
-                Inside the loop, we'll do two things:
+                Inside this loop, we do two things:
 
                     1. Record the current position of the particles, to be reported out
-                    2. Move the particles
+                    2. Move the particles to their next position
 
                 We'll do these two things first for the left particles, and then for the right particles
 
                 We've optimized the initialization and termination expressions of the for-loops below
-                so that we only examine particles still in the chamber
+                so that we only record and shift particles still in the chamber.
+
+                The logic for handling left and right moving particles is very similar, but differs just enough to
+                not warrant trying to consolidate the two sections into a single method.
             */
 
 
             // Initialize this current snapshot to be all periods ('.');
-            // We'll overwrite certain spots within the loops, as appropriate
+            // We'll subsequently overwrite certain elements of the array with 'X'
             Arrays.fill(currentParticleLocationSnapshot, ASCII_PERIOD);
 
             // LEFT Particles
             // Record a snapshot of the left-movers still in the chamber
             // Then shift them
-            final boolean processLeftMoving = skipableLeftMovingParticles < activeLeftMovingParticles;
-            if (processLeftMoving) {
+            final boolean doProcessLeftMoving = skipableLeftMovingParticles < activeLeftMovingParticles;
+            if (doProcessLeftMoving) {
                 for (int i = skipableLeftMovingParticles; i < activeLeftMovingParticles; i++) {
 
                     // Record the location for the snapshot
@@ -117,7 +121,9 @@ public class AnimateParticles {
                     currentParticleLocationSnapshot[particleLocation] = ASCII_X_INDICATOR;
 
                     // Shift the  particle, to the left (by *subtracting* speed value)
-                    // If the particle has left the chamber, mark it as such, and increment the 'skipped' counter
+                    // IF after the move the particle is no longer in the chamber,
+                    // THEN mark it as such, and increment the 'skipped' counter
+                    // In any event, record the new location of the particle
                     int newPosition = particleLocation - speed;
                     if (newPosition < 0) {
                         newPosition = OUT_OF_CHAMBER;
@@ -131,17 +137,19 @@ public class AnimateParticles {
             // Record a snapshot of the right-movers still in the chamber
             //    (These recordings may coincide with left-moving particles, recorded above.)
             // Then shift them
-            final boolean processRightMoving = skipableRightMovingParticles < activeRightMovingParticles;
-            if (processRightMoving) {
-                final int numLeftToProcess = activeRightMovingParticles - skipableRightMovingParticles;
-                for (int i = 0; i < numLeftToProcess; i++) {
+            final boolean doProcessRightMoving = skipableRightMovingParticles < activeRightMovingParticles;
+            if (doProcessRightMoving) {
+                final int numberRemainingToProcess = activeRightMovingParticles - skipableRightMovingParticles;
+                for (int i = 0; i < numberRemainingToProcess; i++) {
 
                     // Record the location for the snapshot
                     final int particleLocation = rightMovingArray[i];
                     currentParticleLocationSnapshot[particleLocation] = ASCII_X_INDICATOR;
 
                     // Shift the particle, to the right (by *adding* speed value)
-                    // If the particle has left the chamber, mark it as such, and increment the 'skipped' counter
+                    // IF after the move the particle is no longer in the chamber,
+                    // THEN mark it as such, and increment the 'skipped' counter
+                    // In any event, record the new location of the particle
                     int newPosition = particleLocation + speed;
                     if (newPosition >= inputStringLength) {
                         newPosition = OUT_OF_CHAMBER;
@@ -151,10 +159,11 @@ public class AnimateParticles {
                 }
             }
 
+            // Add the new snapshot to running list of snapshots
             resultsAtEachSnapShot.add(new String(currentParticleLocationSnapshot));
         }
 
-        // At the very end here, add a final "all done" snapshot to the return results
+        // At the very end here, add a final "all done" snapshot to the running list of snapshots
         Arrays.fill(currentParticleLocationSnapshot, ASCII_PERIOD);
         resultsAtEachSnapShot.add(new String(currentParticleLocationSnapshot));
 
